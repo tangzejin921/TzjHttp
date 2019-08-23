59a2fed6d3ffb1d76c7e756354e1c688cf9223f9
tangzejin921 <tangzejin921@qq.com>
2019-08-23 19:34:32 +0800

    cookie改为更具host

--- library/src/main/java/com/tzj/http/http/StaticCookie.java
+++ library/src/main/java/com/tzj/http/http/StaticCookie.java
@@ -1,7 +1,9 @@
 package com.tzj.http.http;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import okhttp3.Cookie;
 import okhttp3.CookieJar;
@@ -11,17 +13,27 @@ import okhttp3.HttpUrl;
  * Cookie 静态保存
  */
 public class StaticCookie implements CookieJar {
-    public static final List<Cookie> cookies = new ArrayList<>();
+    public static final Map<String,List<Cookie>> cookies = new HashMap<>();
 
     @Override
-    public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
-        if (cookies != null){
-            StaticCookie.cookies.addAll(cookies);
+    public void saveFromResponse(HttpUrl url, List<Cookie> list) {
+        List<Cookie> temp = cookies.get(url.host());
+        if (temp == null){
+            temp = new ArrayList<>();
         }
+        if (list != null){
+            temp.addAll(list);
+        }
+        cookies.put(url.host(),temp);
     }
 
     @Override
     public List<Cookie> loadForRequest(HttpUrl url) {
-        return cookies;
+        List<Cookie> list = cookies.get(url.host());
+        if (list == null){
+            list = new ArrayList<>();
+            cookies.put(url.host(),list);
+        }
+        return list;
     }
 }
